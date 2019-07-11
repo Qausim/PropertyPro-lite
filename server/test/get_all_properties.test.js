06b40b0d44beddfbe37ae117c99394b459c981f0
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-07-11 11:57:24 +0100

    test(agent address): add agent address to get properties test cases

--- server/test/get_all_properties.test.js
+++ server/test/get_all_properties.test.js
@@ -137,6 +137,7 @@ export default () => {
       expect(res.body.data[0]).to.have.property('ownerName');
       expect(res.body.data[0]).to.have.property('ownerEmail');
       expect(res.body.data[0]).to.have.property('ownerPhoneNumber');
+      expect(res.body.data[0]).to.have.property('ownerAddress');
       expect(res.body.data[0].id).to.equal(propertyEntries[0].id);
       expect(res.body.data[0].status).to.equal(propertyEntries[0].status);
       expect(res.body.data[0].type).to.equal(propertyEntries[0].type);
@@ -148,6 +149,7 @@ export default () => {
       expect(res.body.data[0].ownerName).to.equal(`${agent.firstName} ${agent.lastName}`);
       expect(res.body.data[0].ownerEmail).to.equal(agent.email);
       expect(res.body.data[0].ownerPhoneNumber).to.equal(agent.phoneNumber);
+      expect(res.body.data[0].ownerAddress).to.equal(agent.address);
       expect(res.body.data[1]).to.have.property('id');
       expect(res.body.data[1]).to.have.property('status');
       expect(res.body.data[1]).to.have.property('type');
@@ -161,6 +163,7 @@ export default () => {
       expect(res.body.data[1]).to.have.property('ownerName');
       expect(res.body.data[1]).to.have.property('ownerEmail');
       expect(res.body.data[1]).to.have.property('ownerPhoneNumber');
+      expect(res.body.data[1]).to.have.property('ownerAddress');
       expect(res.body.data[1].id).to.equal(propertyEntries[1].id);
       expect(res.body.data[1].status).to.equal(propertyEntries[1].status);
       expect(res.body.data[1].type).to.equal(propertyEntries[1].type);
@@ -172,6 +175,7 @@ export default () => {
       expect(res.body.data[1].ownerName).to.equal(`${agent.firstName} ${agent.lastName}`);
       expect(res.body.data[1].ownerEmail).to.equal(agent.email);
       expect(res.body.data[1].ownerPhoneNumber).to.equal(agent.phoneNumber);
+      expect(res.body.data[1].ownerAddress).to.equal(agent.address);
     });
   });
 
