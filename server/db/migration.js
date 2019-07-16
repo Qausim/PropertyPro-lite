eabc9f94a1c160903e90fe2816a5c6104dd9530b
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-07-16 04:17:00 +0100

    feature(admin): insert admin into database
    [Start #167309858]

--- server/db/migration.js
+++ server/db/migration.js
@@ -1,12 +1,31 @@
+import bcrypt from 'bcrypt';
+
 import dbConnection from './database';
+
+
+const insertAdmin = async (usersTable) => {
+  const adminEmail = process.env.ADMIN_EMAIL;
+  const adminPassword = process.env.ADMIN_PWD;
+  const adminPhone = process.env.ADMIN_PHONE_NUMBER;
+  const adminAddress = process.env.ADMIN_ADDRESS;
+  const hashedPassword = await new Promise((resolve, reject) => {
+    bcrypt.hash(adminPassword, 10, (error, hash) => {
+      if (error) reject(error);
+      resolve(hash);
+    });
+  });
+  await dbConnection.dbConnect(`INSERT INTO ${usersTable} (email, password, first_name, last_name,
+    is_admin, phone_number, address) VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT DO NOTHING;`,
+  [adminEmail, hashedPassword, 'Olawumi', 'Yusuff', true, adminPhone, adminAddress]);
+};
 
 const userTableCreationQuery = `
   CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(250) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    password VARCHAR(250) NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE,
     is_agent BOOLEAN DEFAULT FALSE,
     phone_number VARCHAR(11),
@@ -18,9 +37,9 @@ const userTestTableCreationQuery = `
   CREATE TABLE IF NOT EXISTS users_test (
     id BIGSERIAL PRIMARY KEY NOT NULL,
     email VARCHAR(150) UNIQUE NOT NULL,
+    password VARCHAR(250) NOT NULL,
     first_name VARCHAR(50) NOT NULL,
     last_name VARCHAR(50) NOT NULL,
-    password VARCHAR(250) NOT NULL,
     is_admin BOOLEAN DEFAULT FALSE,
     is_agent BOOLEAN DEFAULT FALSE,
     phone_number VARCHAR(11),
@@ -64,10 +83,12 @@ export default {
   async createTable() {
     await dbConnection.dbConnect(userTableCreationQuery);
     await dbConnection.dbConnect(propertyTableCreationQuery);
+    await insertAdmin('users');
   },
 
   async createTestTables() {
     await dbConnection.dbConnect(userTestTableCreationQuery);
     await dbConnection.dbConnect(propertyTestTableCreationQuery);
+    await insertAdmin('users_test');
   },
 };
