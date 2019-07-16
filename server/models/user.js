9a40cb2c69738db9ec2975f0b3166bc8de5eab7f
Olawumi Yusuff <qauzeemyusuff2@gmail.com>
2019-07-16 08:36:41 +0100

    feature(signup): neglect is_admin field in signup requests

--- server/models/user.js
+++ server/models/user.js
@@ -1,6 +1,6 @@
 export default class User {
   constructor(id, email, firstName, lastName, password, phoneNumber,
-    address, isAdmin, isAgent) {
+    address, isAgent) {
     this.id = id;
     this.email = email;
     this.firstName = firstName;
@@ -8,7 +8,7 @@ export default class User {
     this.password = password;
     this.phoneNumber = phoneNumber || null;
     this.address = address || null;
-    this.isAdmin = isAdmin || false;
+    this.isAdmin = false;
     this.isAgent = isAgent || false;
     this.token = null;
   }
